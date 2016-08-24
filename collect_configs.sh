535d3e64aa94026f973651dcd3c5a88f6629703a
Tim Bachofen <tim.bachofen@gmail.com>
2016-08-24 23:49:58 +0200

    refactored collect_configs

--- collect_configs.sh
+++ collect_configs.sh
@@ -11,18 +11,16 @@ cp $HOME/.toprc ./home/toprc
 cp $HOME/.fehbg ./home/fehbg
 
 rm -r ./home/config
-cp -r /home/$USER/.config/base16-shell ./home/config/base16-shell
+cp -r $HOME/.config/base16-shell ./home/config/base16-shell
 
 rm -r ./home/i3
 cp -r $HOME/.i3 ./home/i3
 
 rm -r ./home/vim
-cp -r /home/$USER/.vim ./home
+cp -r $HOME/.vim ./home
 mv ./home/.vim ./home/vim
 
 rm -r ./etc/X11
 cp -r /etc/X11/xorg.conf.d/ ./etc
 
-add nginx config
-
-generate xmodmap and add .xbindkeysrc.scm
+# TODO add nginx config
