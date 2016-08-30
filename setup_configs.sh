6ad2886e66eac0dc79dcaefe2c481684f183db8c
Tim Bachofen <tim.bachofen@gmail.com>
2016-08-30 20:02:06 +0200

    added bash login/logout scripts, added sudo installation, cosmetics, started rvm setup

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,35 +1,44 @@
 #!/usr/bin/bash
 
-# with pacman -S $(file) can all the files be installed
-# TODO maybe add sudo
-
+# TODO setup laptop specific stuff
 echo "Setting up laptop? [y/n]"
 read LAPTOP
 
 echo "Prepare Setup..."
-cp ./etc/pacman.conf /etc
-su -c "pacman -S git curl"
+# setup sudo
+su -c "pacman -S sudo"
+su -c "visudo"
+
+# setup pacman
+sudo cp ./etc/pacman.conf /etc
+sudo pacman -Syu
+
+# setup utils needed for installation
+sudo pacman -S git curl
 cp ./home/gitconfig $HOME/.gitconfig
 echo "...done!"
 
 echo "X11 Setup..."
-su -c "pacman -S xorg-xinit xorg-server xorg-xrandr xorg-xmodmap xbindkeys"
-xmodmap -pke > ~/.Xmodmap # generate keycodes
-cp ./home/xbindkeysrc $HOME/.xbindkeysrc.scm # bind keycodes
+sudo pacman -S xorg-xinit xorg-server xorg-xrandr
 
 if [[ "$LAPTOP" -eq "y" ]]; then
-   cp -r ./etc/X11/xorg.conf.d /etc/X11/xorg.conf.d # setup evdev, trackball, trackpad
+   sudo pacman -S xorg-xmodmap xbindkeys
+   xmodmap -pke > ~/.Xmodmap # generate keycodes
+   cp ./home/xbindkeysrc $HOME/.xbindkeysrc # bind keycodes
+   sudo cp -r ./etc/X11/xorg.conf.d /etc/X11/xorg.conf.d # setup evdev, trackball, trackpad
 fi
 
 # TODO query available monitors and generate xorg.conf
 echo "...done!"
 
 echo "Bash Setup..."
-su -c "pacman -S xterm xorg-xrdb xinit bash-completion"
+sudo pacman -S xterm xorg-xrdb xinit bash-completion
 cp ./home/bash_profile $HOME/.bash_profile
 cp ./home/xinitrc $HOME/.xinitrc
 cp ./home/Xresources $HOME/.Xresources
 cp ./home/bashrc $HOME/.bashrc
+cp ./home/bash_login $HOME/.bash_login
+cp ./home/bash_logout $HOME/.bash_logout
 cp ./home/toprc $HOME/.toprc
 
 mkdir -r $HOME/.config
@@ -37,13 +46,13 @@ cp -r ./home/config/base16-shell $HOME/.config
 echo "...done!"
 
 echo "GUI Setup..."
-su -c "pacman -S i3 dmenu feh alsa-utils ttf-dejavu"
+sudo pacman -S i3 dmenu feh alsa-utils ttf-dejavu
 cp -r ./home/i3 $HOME/.i3
 cp -r ./home/fehbg $HOME/.fehbg
 echo "...done!"
 
 echo "Installing utilities..."
-pacman -S unzip unrar keepass chromium vlc scrot ntfs-3g udisks2 udevil xclip
+sudo pacman -S unzip unrar keepass chromium vlc scrot ntfs-3g udisks2 udevil xclip
 echo "...done!"
 
 echo "Setup Yaourt..."
@@ -54,24 +63,37 @@ git clone https://aur.archlinux.org/yaourt.git /tmp/yaourt
 echo "...done!"
 
 # Install AUR packages
-yaourt -S folingathome
+if [[ "$LAPTOP" -eq "n" ]]; then
+   yaourt -S folingathome
+fi
 
 echo "VIM Setup..."
-pacman -S vim
+sudo pacman -S vim
 
+# install vim package manager
 mkdir -p $HOME/.vim/autoload
 curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+# install packages
 mkdir -p $HOME/.vim/bundle
-git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
-git clone git://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone git://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-git clone git://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone git://github.com/tpope/vim-fugitive.git $HOME/.vim/bundle/vim-fugitive
+git clone git://github.com/vim-airline/vim-airline $HOME/.vim/bundle/vim-airline
+git clone git://github.com/vim-airline/vim-airline-themes $HOME/.vim/bundle/vim-airline-themes
+git clone git://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
+vim -u NONE -c "helptags vim-fugitive/doc" -c "helptags vim-airline/doc" -c "helptags vim-airline-themes/doc" -c "helptags ctrlp.vim/doc" -c q
 
+# install color suite
 mkdir -p $HOME/.vim/colors
 git clone git://github.com/chriskempson/base16-vim.git $HOME/.vim/colors
-
-vim -u NONE -c "helptags vim-fugitive/doc" -c "helptags vim-airline/doc" -c "helptags vim-airline-themes/doc" -c "helptags ctrlp.vim/doc" -c q
 echo "...done!"
 
+echo "LaTex Setup..."
+sudo pacman -S texlive-core texlive-bin texlive-bibtexextra texlive-latexextra biber xelatex
+
+
 # TODO implement dev setup
+echo "RVM Setup..."
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+source $HOME/.bash_login
+
+echo "...done!"
