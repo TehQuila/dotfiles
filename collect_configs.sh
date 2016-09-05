860a80d11b31f636c27f67291f283ae1210f26b7
Tim Bachofen <tim.bachofen@gmail.com>
2016-09-05 10:23:11 +0200

    updated config collection script

--- collect_configs.sh
+++ collect_configs.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/bash
 
+cp /etc/pacman.conf ./etc
+
+cp $HOME/.gitconfig ./home/gitconfig
+cp $HOME/.curlrc ./home/curlrc
+
+cp $HOME/.xbindkeysrc ./home/xbindkeysrc
+cp -r /etc/X11/xorg.conf.d ./etc/X11/xorg.conf.d
+
 cp $HOME/.bash_profile ./home/bash_profile
-cp $HOME/.bashrc ./home/bashrc
-cp $HOME/.bashrc ./home/bashrc
-cp $HOME/.vimrc ./home/vimrc
 cp $HOME/.xinitrc ./home/xinitrc
 cp $HOME/.Xresources ./home/Xresources
+cp $HOME/.bashrc ./home/bashrc
+cp $HOME/.vimrc ./home/vimrc
+cp $HOME/.bash_login ./home/bash_login
+cp $HOME/.bash_logout ./home/bash_logout
 cp $HOME/.toprc ./home/toprc
-
-cp $HOME/.fehbg ./home/fehbg
 
 rm -r ./home/config
 cp -r $HOME/.config/base16-shell ./home/config/base16-shell
 
 rm -r ./home/i3
 cp -r $HOME/.i3 ./home/i3
+cp $HOME/.fehbg ./home/fehbg
 
-rm -r ./home/vim
-cp -r $HOME/.vim ./home
-mv ./home/.vim ./home/vim
-
-rm -r ./etc/X11
-cp -r /etc/X11/xorg.conf.d/ ./etc
-
-# TODO add nginx config
+cp -r $HOME/.vimrc ./home/vimrc
