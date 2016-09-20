6971b1c43eb3c80fbc863c0da05ae4074be68669
Tim Bachofen <tim.bachofen@gmail.com>
2016-09-20 12:43:39 +0200

    removed unnecessary output

--- setup_configs.sh
+++ setup_configs.sh
@@ -5,7 +5,6 @@
 echo "Setting up laptop? [y/n]"
 read LAPTOP
 
-echo "Preparing Setup..."
 cp ./home/gitconfig $HOME/.gitconfig
 cp ./home/curlrc $HOME/.curlrc
 
@@ -13,12 +12,8 @@ sudo cp ./etc/pacman.conf /etc
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman -Syu --noconfirm --needed
-echo "...done!"
 
 # TODO query available monitors and generate xorg.conf
-
-echo "Graphic Stack Setup..."
-sudo pacman -S xorg-xrandr --noconfirm
 
 controller=$(lspci | grep VGA)
 if [[ "$(echo $controller | grep Intel -c)" ]]; then
@@ -29,14 +24,8 @@ elif [[ "$(echo $controller | grep Nvidia -c)" ]]; then
    sudo pacman -S xf86-video-nouveau --noconfirm
 fi
 
-while read -r line; do
-   if [[ $line =~ ([A-Z]+[1-9])[[:space:]]connected ]]; then
-      echo ${BASH_REMATCH[1]};
-   fi
-done < <(xrandr)
-echo "..done!"
+sudo pacman -S xorg-xrandr --noconfirm
 
-echo "X11 Setup..."
 sudo pacman -S xterm xorg-xrdb xorg-xinit xorg-server  --noconfirm
 
 cp ./home/xinitrc $HOME/.xinitrc
@@ -49,9 +38,6 @@ if [[ "$LAPTOP" -eq "y" ]]; then
    sudo cp ./etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d
 fi
 
-echo "...done!"
-
-echo "Bash Setup..."
 sudo pacman -S bash-completion --noconfirm
 cp ./home/bash_profile $HOME/.bash_profile
 cp ./home/bashrc $HOME/.bashrc
@@ -63,35 +49,26 @@ fi
 
 git clone git://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
 base16_default_dark
-echo "...done!"
 
-echo "GUI Setup..."
 sudo pacman -S i3 dmenu feh alsa-utils ttf-dejavu --noconfirm
 cp -r ./home/i3 $HOME/.i3
 cp -r ./home/fehbg $HOME/.fehbg
-echo "...done!"
 
-echo "Installing utilities..."
 sudo pacman -S openssh unzip unrar keepass chromium vlc scrot ntfs-3g udisks2 udevil xclip --noconfirm
-echo "...done!"
 
-echo "Setting up Bluetooth..."
+# do i really need that?
 sudo pacman -S modprobe btusb --noconfirm
 modprobe btusb
-echo "...done!"
 
-echo "Setup Yaourt..."
 git clone https://aur.archlinux.org/package-query.git /tmp/package-query
 git clone https://aur.archlinux.org/yaourt.git /tmp/yaourt
 (cd /tmp/package-query && exec makepkg -si)
 (cd /tmp/yaourt && exec makepkg -si)
-echo "...done!"
 
 if [[ "$LAPTOP" -eq "n" ]]; then
    yaourt -S folingathome
 fi
 
-echo "VIM Setup..."
 sudo pacman -S vim --noconfirm
 cp ./home/vimrc $HOME/.vimrc
 
@@ -107,17 +84,12 @@ vim -u NONE -c "helptags vim-airline/doc" -c "helptags vim-airline-themes/doc" -
 mkdir -p $HOME/.vim/colors
 git clone git://github.com/chriskempson/base16-vim.git /tmp
 mv /tmp/base16-vim/colors/* $HOME/.vim/colors
-echo "...done!"
 
-echo "LaTex Setup..."
 sudo pacman -S texlive-core texlive-bin texlive-bibtexextra texlive-latexextra biber --noconfirm
-echo "...done!"
 
-echo "RVM Setup..."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable
 cp ./home/rvmrc $HOME/.rvmrc
 
 rvm install ruby
 rvm --default use ruby
-echo "...done!"
