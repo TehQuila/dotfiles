c41e8c873641d859e7ead3aa6a151dae2dc2593b
Tim Bachofen <tim.bachofen@gmail.com>
2016-09-24 11:41:57 +0200

    created optional section for setup

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-# TODO think about different optional software
-
 cp ./home/gitconfig $HOME/.gitconfig
 cp ./home/curlrc $HOME/.curlrc
 
@@ -50,19 +48,10 @@ for i in ${!screens[@]}; do
    echo "" | sudo tee --append $x_config
 done
 
-sudo pacman -S xterm xorg-xrdb xorg-xinit xorg-server  --noconfirm
+sudo pacman -S xterm xorg-xrdb xorg-xinit xorg-server --noconfirm
 
 cp ./home/xinitrc $HOME/.xinitrc
 cp ./home/Xresources $HOME/.Xresources
-
-echo "Setting up laptop? [y/n]"
-read laptop
-if [[ "$laptop" -eq "y" ]]; then
-   sudo pacman -S xorg-xmodmap xbindkeys --noconfirm
-   xmodmap -pke > ~/.Xmodmap # generate keycodes
-   cp ./home/xbindkeysrc $HOME/.xbindkeysrc # bind keycodes
-   sudo cp ./etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d
-fi
 
 sudo pacman -S bash-completion --noconfirm
 cp ./home/bash_profile $HOME/.bash_profile
@@ -72,17 +61,14 @@ cp ./home/toprc $HOME/.toprc
 if [[ ! -d "$HOME/.config" ]]; then
    mkdir $HOME/.config
 fi
-
 git clone git://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
 base16_default_dark
 
-sudo pacman -S i3 dmenu feh alsa-utils ttf-dejavu --noconfirm
+sudo pacman -S i3 dmenu feh ttf-dejavu --noconfirm
 cp -r ./home/i3 $HOME/.i3
 cp -r ./home/fehbg $HOME/.fehbg
 
-sudo pacman -S openssh unzip unrar keepass chromium vlc scrot ntfs-3g udisks2 udevil --noconfirm
-
-sudo pacman -S modprobe btusb --noconfirm
+sudo pacman -S openssh unzip unrar keepass firefox vlc scrot ntfs-3g udisks2 udevil modprobe btusb --noconfirm
 modprobe btusb
 
 git clone https://aur.archlinux.org/package-query.git /tmp/package-query
@@ -90,17 +76,11 @@ git clone https://aur.archlinux.org/yaourt.git /tmp/yaourt
 (cd /tmp/package-query && exec makepkg -si)
 (cd /tmp/yaourt && exec makepkg -si)
 
-if [[ "$laptop" -eq "n" ]]; then
-   yaourt -S foldingathome
-fi
-
-sudo pacman -S vim --noconfirm
+sudo pacman -S gvim --noconfirm
 cp ./home/vimrc $HOME/.vimrc
 
-mkdir -p $HOME/.vim/autoload
-curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-mkdir -p $HOME/.vim/bundle
 git clone git://github.com/vim-airline/vim-airline $HOME/.vim/bundle/vim-airline
 git clone git://github.com/vim-airline/vim-airline-themes $HOME/.vim/bundle/vim-airline-themes
 git clone git://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
@@ -112,24 +92,49 @@ mv /tmp/base16-vim/colors/* $HOME/.vim/colors
 
 sudo pacman -S texlive-core texlive-bin texlive-bibtexextra texlive-latexextra biber --noconfirm
 
-# TODO implement printer setup
-# sudo pacman -S cups avahi nss-mdns
-# systemctl enable avahi-daemon.service
-# systemctl enable org.cups.cupsd.service
-# systemctl start avahi-daemon.service
-# systemctl start org.cups.cupsd.service
-
-# sudo lpinfo -v
-# echo "Enter Printer name: "
-# read queue_name
-# echo "Paste Printer URI: "
-# read uri
-
-# sudo lpadmin -p $queue_name -E -v "$uri"
-# sudo lpoptions -d $queue_name
-# sudo cupsenable $queue_name
-# sudo cupsaccept $queue_name
-
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable
 cp ./home/rvmrc $HOME/.rvmrc
+
+# TODO think about different optional software
+echo "Setup a Printer? [y/n]"
+read printer
+if [[ "$printer" -eq "y" ]]; then
+   sudo pacman -S cups avahi nss-mdns --noconfirm
+   systemctl enable avahi-daemon.service
+   systemctl enable org.cups.cupsd.service
+   systemctl start avahi-daemon.service
+   systemctl start org.cups.cupsd.service
+
+   sudo lpinfo -v
+   echo "Enter Printer name: "
+   read queue_name
+   echo "Paste Printer URI: "
+   read uri
+
+   sudo lpadmin -p $queue_name -E -v "$uri"
+   sudo lpoptions -d $queue_name
+   sudo cupsenable $queue_name
+   sudo cupsaccept $queue_name
+fi
+
+echo "Setup Special Hardware Buttons? [y/n]"
+read buttons
+if [[ "$buttons" -eq "y" ]]; then
+   sudo pacman -S xorg-xmodmap xbindkeys --noconfirm
+   xmodmap -pke > ~/.Xmodmap # generate keycodes
+   cp ./home/xbindkeysrc $HOME/.xbindkeysrc # bind keycodes
+   sudo cp ./etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d
+fi
+
+echo "Setup audio? [y/n]"
+read audio
+if [[ "$audio" -eq "y" ]]; then
+   sudo pacman -S alsa-utils --noconfirm
+fi
+
+echo "Setup folgin@home? [y/n]"
+read fah
+if [[ "$fah" -eq "n" ]]; then
+   yaourt -S foldingathome --noconfirm
+fi
