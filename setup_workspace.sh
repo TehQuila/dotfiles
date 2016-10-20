4f070576d4af15e2797aa5fc5bf838cc07d878e2
Tim Bachofen <tim.bachofen@gmail.com>
2016-10-20 08:48:56 +0200

    remove walking dev repo pull

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -10,7 +10,6 @@ read -n1 -r -p "Press any key to continue..." key
 git clone git@github.com:TehQuila/lettertemplate.git $HOME/workspace
 git clone git@github.com:TehQuila/curriculumvitae.git $HOME/workspace
 git clone git@github.com:TehQuila/projecteuler.git $HOME/workspace
-git clone git@github.com:MarcoGlauser/TheWalkingDev.git $HOME/workspace
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable
