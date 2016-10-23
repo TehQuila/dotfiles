37e9fe38c400d453f5a5b35c61e1d4ec71e03d20
Tim Bachofen <tim.bachofen@gmail.com>
2016-10-23 13:35:53 +0200

    added bluetooth section, fixed install, x11 setup

--- setup_base.sh
+++ setup_base.sh
@@ -13,8 +13,9 @@ git clone https://aur.archlinux.org/yaourt.git /tmp/yaourt
 (cd /tmp/yaourt && exec makepkg -si)
 
 # Install packages
-# TODO is not working
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xrdb xterm bash-completion openssh i3 dmenu feh ttf-dejavu unzip unrar keepass firefox scrot ntfs-3g udisks2 udevil modprobe btusb gvim texlive-core texlive-bin texlive-bibtexextra texlive-latexextra biber --noconfirm
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xrdb xterm bash-completion openssh i3-wm i3lock i3status dmenu feh ttf-dejavu unzip unrar keepass firefox scrot ntfs-3g udisks2 udevil gvim texlive-core texlive-bin texlive-bibtexextra texlive-latexextra biber --noconfirm
+
+# TODO setup firefox (settings/plugins etc)
 
 # Install optional packages
 sudo pacman -S alsa-utils
@@ -37,11 +38,10 @@ git clone https://github.com/vim-airline/vim-airline $HOME/.vim/bundle/vim-airli
 git clone https://github.com/vim-airline/vim-airline-themes $HOME/.vim/bundle/vim-airline-themes
 git clone https://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
 
-vim -u NONE -c "helptags vim-airline/doc" -c "helptags vim-airline-themes/doc" -c "helptags ctrlp.vim/doc" -c q
+vim -u NONE -c "helptags $HOME/.vim/bundle/vim-airline/doc" -c "helptags $HOME/.vim/bundle/vim-airline-themes/doc" -c "helptags $HOME/.vim/bundle/ctrlp.vim/doc" -c q
 
 # Execute setup commands
 modprobe btusb
-base16_default-dark
 
 # Copy configs
 cp ./home/gitconfig $HOME/.gitconfig
@@ -61,12 +61,25 @@ sudo cp ./etc/X11/xorg.conf.d/01-mouse.conf /etc/X11/xorg.conf.d
 echo "Setup Trackpoint/Touchpad/HW Button configs? [y/n]"
 read laptop
 if [[ "$laptop" -eq "y" ]]; then
-   cp ./etc/X11/xorg.conf.d/10-trackpoint.conf /etc/X11/xorg.conf.d
-   cp ./etc/X11/xorg.conf.d/11-touchpad.conf /etc/X11/xorg.conf.d
+   sudo cp ./etc/X11/xorg.conf.d/10-trackpoint.conf /etc/X11/xorg.conf.d
+   sudo cp ./etc/X11/xorg.conf.d/11-touchpad.conf /etc/X11/xorg.conf.d
    sudo pacman -S xorg-xmodmap xbindkeys --noconfirm
-   xmodmap -pke > ~/.Xmodmap # generate keycodes
+   xmodmap -pke > $HOME/.Xmodmap # generate keycodes
    cp ./home/xbindkeysrc $HOME/.xbindkeysrc # bind keycodes
 fi
+
+# TODO Setup Bluetooth
+echo "Setup Bluetooth? [y/n]"
+read bluetooth
+if [[ "$bluetooth" -eq "y" ]]; then
+   sudo pacman -S bluez bluez-utils dbus alsa-plugins pulseaudio pulseaudio-alsa
+   sudo systemctl enable dbus
+   sudo systemctl start dbus
+   dbus-daemon --session --fork
+
+   sudo systemctl start bluetooth.service
+fi
+
 
 # Setup Monitors
 controller=$(lspci | grep -i vga)
@@ -80,30 +93,34 @@ else
    sudo pacman -S xf86-video-fbdev --noconfirm
 fi
 
-monitor_config="/etc/X11/xorg.conf.d/20-monitors.conf"
-screens=()
-while read -r line; do
-   if [[ $line =~ ([A-Z]+[1-9])[[:space:]]connected ]]; then
-      screens+=("${BASH_REMATCH[1]}")
-   fi
-done < <(xrandr)
+echo "Setup Monitors? [y/n]"
+read monitors
+if [[ "$monitors" -eq "y" ]]; then
+   monitor_config="/etc/X11/xorg.conf.d/20-monitors.conf"
+   screens=()
+   while read -r line; do
+      if [[ $line =~ ([A-Z]+[1-9])[[:space:]]connected ]]; then
+         screens+=("${BASH_REMATCH[1]}")
+      fi
+   done < <(xrandr)
 
-echo "Choose primary screen: "
-for i in ${!screens[@]}; do
-   echo "($i) ${screens[i]}"
-done
-read primary
+   echo "Choose primary screen: "
+   for i in ${!screens[@]}; do
+      echo "($i) ${screens[i]}"
+   done
+   read primary
 
-for i in ${!screens[@]}; do
-   echo "Section \"Monitor\"" | sudo tee --append $monitor_config
-   echo "   Identifier \"${screens[i]}\"" | sudo tee --append $monitor_config
-   if [[ $i -eq $primary ]]; then
-      echo "   Option \"Primary\" \"true\"" | sudo tee --append $monitor_config
-   else
-      echo "Screen ${screens[i]} right or left of primary? [LeftOf/RightOf]"
-      read side
-      echo "Option \"$side\" \"${screens[primary]}\"" | sudo tee --append $monitor_config
-   fi
-   echo "EndSection" | sudo tee --append $monitor_config
-   echo "" | sudo tee --append $monitor_config
-done
+   for i in ${!screens[@]}; do
+      echo "Section \"Monitor\"" | sudo tee --append $monitor_config
+      echo "   Identifier \"${screens[i]}\"" | sudo tee --append $monitor_config
+      if [[ $i -eq $primary ]]; then
+         echo "   Option \"Primary\" \"true\"" | sudo tee --append $monitor_config
+      else
+         echo "Screen ${screens[i]} right or left of primary? [LeftOf/RightOf]"
+         read side
+         echo "Option \"$side\" \"${screens[primary]}\"" | sudo tee --append $monitor_config
+      fi
+      echo "EndSection" | sudo tee --append $monitor_config
+      echo "" | sudo tee --append $monitor_config
+   done
+fi
