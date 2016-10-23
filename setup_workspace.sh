8ffd64f4e3e9aceab1a602b66f1801cf9e4948ea
Tim Bachofen <tim.bachofen@gmail.com>
2016-10-23 22:43:05 +0200

    updated workspace script

--- setup_workspace.sh
+++ setup_workspace.sh
@@ -7,15 +7,17 @@ firefox -new-window https://github.com/settings/keys
 
 read -n1 -r -p "Press any key to continue..." key
 
-git clone git@github.com:TehQuila/lettertemplate.git $HOME/workspace
-git clone git@github.com:TehQuila/curriculumvitae.git $HOME/workspace
-git clone git@github.com:TehQuila/projecteuler.git $HOME/workspace
+git clone git@github.com:TehQuila/lettertemplate.git $HOME/workspace/lettertemplate
+git clone git@github.com:TehQuila/curriculumvitae.git $HOME/workspace/curriculumvitae
+git clone git@github.com:TehQuila/projecteuler.git $HOME/workspace/projecteuler
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+sudo gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable
+sudo gpasswd -a $USER rvm
 cp ./home/rvmrc $HOME/.rvmrc
+
 
 sudo pacman -S python nodejs npm --noconfirm
 
 yaourt -S webstorm datagrip intellij-idea-ultimate-edition clion rubymine pycharm-professional android-studio android-tools --noconfirm
-gpasswd -a $USER adbusers
+sudo gpasswd -a $USER adbusers
