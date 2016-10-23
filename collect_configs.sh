e3a189ade87e6f60e54a2fb1fa8b380f58fe8832
Tim Bachofen <tim.bachofen@gmail.com>
2016-10-23 13:58:37 +0200

    updated collecting

--- collect_configs.sh
+++ collect_configs.sh
@@ -8,8 +8,8 @@ cp $HOME/.curlrc ./home/curlrc
 cp $HOME/.xbindkeysrc ./home/xbindkeysrc
 cp /etc/X11/xorg.conf.d/00-keyboard.conf ./etc/X11/xorg.conf.d
 cp /etc/X11/xorg.conf.d/01-mouse.conf ./etc/X11/xorg.conf.d
-[[ -s "/etc/X11/xorg.conf.d/10-trackpoint.conf" ]] && cp /etc/X11/xorg.conf.d/10-trackpoint.conf ./etc/X11/xorg.conf.d
-[[ -s "/etc/X11/xorg.conf.d/11-touchpad.conf" ]] && cp /etc/X11/xorg.conf.d/11-touchpad.conf ./etc/X11/xorg.conf.d
+[[ -a "/etc/X11/xorg.conf.d/10-trackpoint.conf" ]] && cp /etc/X11/xorg.conf.d/10-trackpoint.conf ./etc/X11/xorg.conf.d
+[[ -a "/etc/X11/xorg.conf.d/11-touchpad.conf" ]] && cp /etc/X11/xorg.conf.d/11-touchpad.conf ./etc/X11/xorg.conf.d
 
 cp $HOME/.bash_profile ./home/bash_profile
 cp $HOME/.xinitrc ./home/xinitrc
@@ -21,4 +21,4 @@ cp $HOME/.i3/* ./home/i3
 
 cp $HOME/.vimrc ./home/vimrc
 
-cp $HOME/.rvmrc ./home/rvmrc
+[[ -a "$HOME/.rvmrc" ]] && cp $HOME/.rvmrc ./home/rvmrc
