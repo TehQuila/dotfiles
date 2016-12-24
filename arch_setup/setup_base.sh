30356273225229b32c809a3496aef37a880ca6f0
Tim Bachofen <tim.bachofen@gmail.com>
2016-12-24 12:07:50 +0100

    improved monitor setup

--- arch_setup/setup_base.sh
+++ arch_setup/setup_base.sh
@@ -143,40 +143,49 @@ fi
 
 read -n1 -p "Setup Monitors? [y/n] " monitors
 if [[ "$monitors" == "y" ]]; then
-   monitor_config="/etc/X11/xorg.conf.d/20-monitors.conf"
    screens=()
    while read -r line; do
-      if [[ $line =~ ([A-Z]+[1-9])[[:space:]]connected ]]; then
+      if [[ $line =~ (([A-Z]+[-])+[1-9])[[:space:]]connected ]]; then
          screens+=("${BASH_REMATCH[1]}")
       fi
    done < <(xrandr)
 
-   echo "Choose primary screen: "
-   for i in${!screens[@]}; do
-      echo "($i) ${screens[i]}"
-   done
-   read primary
+   echo ""
 
    for i in ${!screens[@]}; do
-      echo "Section \"Monitor\"" | sudo tee --append $monitor_config
-      echo "   Identifier \"${screens[i]}\"" | sudo tee --append $monitor_config
-      if [[ $i -eq $primary ]]; then
-         echo "   Option \"Primary\" \"true\"" | sudo tee --append $monitor_config
-      else
-         echo "Screen ${screens[i]} right or left of primary? [LeftOf/RightOf]"
-         read side
-         echo "   Option \"$side\" \"${screens[primary]}\"" | sudo tee --append $monitor_config
-      fi
-      echo "   Option \"DPMS\" \"true\"" | sudo tee --append $monitor_config
-      echo "EndSection" | sudo tee --append $monitor_config
-      echo "" | sudo tee --append $monitor_config
+      echo "($i) ${screens[i]}"
    done
+   read -n1 -p "Choose primary screen: " primary
 
-   echo "Section \"ServerLayout\"" >> sudo tee --append $monitor_config
-   echo "   Identifier \"ServerLayout0\"" >> sudo tee --append $monitor_config
-   echo "   Option \"StandbyTime\" \"0\"" >> sudo tee --append $monitor_config
-   echo "   Option \"SuspendTime\" \"0\"" >> sudo tee --append $monitor_config
-   echo "   Option \"OffTime\" \"0\"" >> sudo tee --append $monitor_config
-   echo "   Option \"BlankTime\" \"0\"" >> sudo tee --append $monitor_config
-   echo "EndSection" >> sudo tee --append $monitor_config
+   echo ""
+
+   {
+      for i in ${!screens[@]}; do
+         sudo echo "Section \"Monitor\"" >&3
+         sudo echo "   Identifier \"${screens[i]}\"" >&3
+         if [[ $i -eq $primary ]]; then
+            sudo echo "   Option \"Primary\" \"true\"" >&3
+         else
+            read -n1 -p "${screens[i]} right or left of ${screens[primary]}? [l/r] " side
+            echo ""
+            if [[ $side == "l" ]]; then
+               sudo echo "   Option \"LeftOf\" \"${screens[primary]}\"" >&3
+            elif [[ $side == "r" ]]; then
+               sudo echo "   Option \"RightOf\" \"${screens[primary]}\"" >&3
+            fi
+         fi
+         sudo echo "   Option \"DPMS\" \"true\"" >&3
+         sudo echo "EndSection" >&3
+         sudo echo "" >&3
+      done
+
+      sudo echo "Section \"ServerLayout\"" >&3
+      sudo echo "   Identifier \"ServerLayout0\"" >&3
+      sudo echo "   Option \"StandbyTime\" \"0\"" >&3
+      sudo echo "   Option \"SuspendTime\" \"0\"" >&3
+      sudo echo "   Option \"OffTime\" \"0\"" >&3
+      sudo echo "   Option \"BlankTime\" \"0\"" >&3
+      sudo echo "EndSection" >&3
+
+   } 3>/etc/X11/xorg.conf.d/20-monitors.conf
 fi
