d4db3bcc0e5c77edaa115cce3e7ff9d62eafeef9
Tim Bachofen <tim.bachofen@gmail.com>
2017-01-09 14:58:22 +0100

    updated config collection

--- config_management/collect_configs.sh
+++ config_management/collect_configs.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/bash
 
-cp /etc/pacman.conf ./etc
-cp /etc/X11/xorg.conf.d/00-keyboard.conf ./etc/X11/xorg.conf.d
-cp /etc/X11/xorg.conf.d/01-mouse.conf ./etc/X11/xorg.conf.d
-[[ -e /etc/X11/xorg.conf.d/10-trackpoint.conf ]] && cp /etc/X11/xorg.conf.d/10-trackpoint.conf ./etc/X11/xorg.conf.d
-[[ -e /etc/X11/xorg.conf.d/11-touchpad.conf ]] && cp /etc/X11/xorg.conf.d/11-touchpad.conf ./etc/X11/xorg.conf.d
-[[ -e $HOME/.xbindkeysrc ]] && cp $HOME/.xbindkeysrc ./home/xbindkeysrc
-cp $HOME/.bash_profile ./home/bash_profile
-cp $HOME/.xinitrc ./home/xinitrc
-cp $HOME/.Xresources ./home/Xresources
-cp $HOME/.bashrc ./home/bashrc
-cp -r $HOME/.i3/* ./home/i3
-cp $HOME/.gitconfig ./home/gitconfig
-cp $HOME/.curlrc ./home/curlrc
-cp $HOME/.toprc ./home/toprc
-cp $HOME/.vimrc ./home/vimrc
-[[ -e $HOME/.rvmrc ]] && cp $HOME/.rvmrc ./home/rvmrc
-cp -r $HOME/.mozilla/* ./home/mozilla
+WORKDIR=$(pwd)
+DIR="${WORKDIR}/config_management"
+
+cp /etc/pacman.conf $DIR/etc
+cp /etc/X11/xorg.conf.d/00-keyboard.conf $DIR/etc/X11/xorg.conf.d
+cp /etc/X11/xorg.conf.d/01-mouse.conf $DIR/etc/X11/xorg.conf.d
+[[ -e /etc/X11/xorg.conf.d/10-trackpoint.conf ]] && cp /etc/X11/xorg.conf.d/10-trackpoint.conf $DIR/etc/X11/xorg.conf.d
+[[ -e /etc/X11/xorg.conf.d/11-touchpad.conf ]] && cp /etc/X11/xorg.conf.d/11-touchpad.conf $DIR/etc/X11/xorg.conf.d
+[[ -e $HOME/.xbindkeysrc ]] && cp $HOME/.xbindkeysrc $DIR/home/xbindkeysrc
+cp $HOME/.bash_profile $DIR/home/bash_profile
+cp $HOME/.xinitrc $DIR/home/xinitrc
+cp $HOME/.Xresources $DIR/home/Xresources
+cp $HOME/.bashrc $DIR/home/bashrc
+cp -r $HOME/.i3/* $DIR/home/i3
+cp $HOME/.gitconfig $DIR/home/gitconfig
+cp $HOME/.curlrc $DIR/home/curlrc
+cp $HOME/.toprc $DIR/home/toprc
+cp $HOME/.vimrc $DIR/home/vimrc
+[[ -e $HOME/.rvmrc ]] && cp $HOME/.rvmrc $DIR/home/rvmrc
+
+FFDIR=".mozilla/firefox"
+PROFILEDIR="2n3roeyr.default"
+cp $HOME/$FFDIR/profiles.ini $DIR/home/mozilla/firefox
+cp $HOME/$FFDIR/$PROFILEDIR/search.json.mozlz4 $DIR/home/mozilla/firefox/$PROFILEDIR
+cp $HOME/$FFDIR/$PROFILEDIR/permissions.sqlite $DIR/home/mozilla/firefox/$PROFILEDIR
+cp $HOME/$FFDIR/$PROFILEDIR/content-prefs.sqlite $DIR/home/mozilla/firefox/$PROFILEDIR
+cp $HOME/$FFDIR/$PROFILEDIR/cert8.db $DIR/home/mozilla/firefox/$PROFILEDIR
+cp $HOME/$FFDIR/$PROFILEDIR/mimeTypes.rdf $DIR/home/mozilla/firefox/$PROFILEDIR
+cp $HOME/$FFDIR/$PROFILEDIR/xulstore.json $DIR/home/mozilla/firefox/$PROFILEDIR
+cp $HOME/$FFDIR/$PROFILEDIR/prefs.js $DIR/home/mozilla/firefox/$PROFILEDIR
+cp -r $HOME/$FFDIR/$PROFILEDIR/extensions $DIR/home/mozilla/firefox/$PROFILEDIR
