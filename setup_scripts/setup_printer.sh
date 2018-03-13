83c2642b0fdb7837f1a702b61d129caad3d98495
Tim Bachofen <tim.bachofen@gmail.com>
2018-03-13 11:24:13 +0100

    updated printer setup for hp laserjet

--- setup_scripts/setup_printer.sh
+++ setup_scripts/setup_printer.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/bash
 
-sudo pacman -S cups avahi nss-mdns --noconfirm
-systemctl enable avahi-daemon
-systemctl enable org.cups.cupsd
-systemctl start avahi-daemon
-systemctl start org.cups.cupsd
+sudo pacman -S cups --noconfirm
+sudo pacman -S hplips --noconfirm
 
-echo "Enter Printer name: "
-read queue_name
+systemctl enable org.cups.cupsd.service
+systemctl start org.cups.cupsd.service
 
-sudo lpinfo -v
-echo "Paste Printer URI: "
-read uri
-
-sudo lpinfo -m
-echo "Paste Printer Model: "
-read model
-
-sudo lpadmin -p "$queue_name" -E -v "$uri" -m "$model"
-sudo lpoptions -d $queue_name
-sudo cupsenable $queue_name
-sudo cupsaccept $queue_name
+sudo hp-setup -i
