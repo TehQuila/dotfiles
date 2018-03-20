f29f0fba6f36b4ea14ae3e52e8c370990cee5779
Tim Bachofen <tim.bachofen@gmail.com>
2018-03-20 15:33:03 +0100

    updated nas setup

--- setup_scripts/setup_nas.sh
+++ setup_scripts/setup_nas.sh
@@ -8,11 +8,11 @@ read -p "Enter samba password: " password
 {
    echo "username=${username}" >&3
    echo "password=${password}" >&3
-} 3>>$HOME/.credentials/BigData
+} 3>>$HOME/.credentials/bigdata
 sudo chmod 600 .credentials
 
-sudo mkdir /mnt/BigData
+sudo mkdir /mnt/bigdata
 {
-   sudo echo "\# BigData" >&3
-   sudo echo "//192.168.0.2/Share      /mnt/BigData    cifs     credentials=$HOME/.credentials/BigData,x-systemd.automount,iocharset=utf8     0 0" >&3
+   sudo echo "\# bigdata" >&3
+   sudo echo "//192.168.178.2/data	/mnt/bigdata	cifs     uid=tim,credentials=/home/tim/.credentials/bigdata,nofail,noauto,x-systemd.automount,x-systemd.requires=network-online.target,x-systemd.device-timeout=10,iocharset=utf8,vers=3.0	0 0" >&3
 } 3>>/etc/fstab
