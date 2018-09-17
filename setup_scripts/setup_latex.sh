2ff47e5142bb51baad640941b0c002af6cc4f616
Tim Bachofen <tim.bachofen@gmail.com>
2018-09-17 10:44:03 +0200

    added todos

--- setup_scripts/setup_latex.sh
+++ setup_scripts/setup_latex.sh
@@ -8,6 +8,9 @@ read -n1 -p "Setup UZH template? [y/n] " uzh
 if [[ "$uzh" == "y" ]]; then
    mkdir -p $HOME/texmf/tex/latex/
    wget https://tug.org/fonts/getnonfreefonts/install-getnonfreefonts > /tmp/install-getnonfreefonts
-   texlua /tmp/install-getnonfreefonts
+   sudo texlua /tmp/install-getnonfreefonts --sys
    sudo getnonfreefonts arial-urw
+   # todo: download uzh template: https://www.cd.uzh.ch/de/vorlagenboerse/downloads.html
+   # mv downloads/Vorlagen_UZH_LaTeX_Publikationen/tex ~/texmf/tex/latex/uzh/
+   # update text filename database: sudo mktexlsr
 fi
